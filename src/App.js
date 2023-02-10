07e2acc51a3a398b464fe851e8329f8a5ed1fc78
Shubhrant05 <shubhrantyadav34366@gmail.com>
2023-02-10 21:16:54 +0530

    initial commits of navbar and routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Routes,Route } from 'react-router-dom';
+import Home from './components/Home/home';
+import About from './components/About/about';
+import { Navbar } from 'react-bootstrap';
+import NavbarHeader from './components/Navbar/navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Routes>
+        <Route path='/home' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/' element={<NavbarHeader />} />
+    </Routes>
+    </>
   );
 }
 
